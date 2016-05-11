39e30e2ce9e9200cd9d76575c73ceaaa0858ee8f
Philip Hutchins <philip@storj.io>
2016-05-11 09:21:12 -0400

    Adding ENV vars to all config values and adding required node modules

--- config/default.js
+++ config/default.js
@@ -5,15 +5,15 @@ const bitcore = require('bitcore-lib');
 
 module.exports = {
   log: {
-    timestamp: true,
-    level: 'debug'
+    timestamp: process.env.LOG_TIMESTAMP || true,
+    level: process.env.LOG_LEVEL || 'debug'
   },
   PORT: process.env.PORT || 3000,
   keys: {
     // default config generates new keys each run... override in config/local.js
-    statusify: new bitcore.PrivateKey().toString('hex')
+    statusify: process.env.KEYS_STATUSIFY || new bitcore.PrivateKey().toString('hex')
   },
   systems: {
-    'data-api': 'http://localhost:3001'
+    'data-api': process.env.SYSTEMS_DATA-API || 'http://localhost:3001'
   }
 };
